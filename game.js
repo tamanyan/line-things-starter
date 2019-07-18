f32ede4f699f7923e9b0b711cb630e2d425ebc6b
Taketo Yoshida <tamanyan.sss@gmail.com>
2019-07-18 16:03:42 +0700

    Update game

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '3c4a02f3-f336-4f17-83cc-0d05d13b1b94';
+const USER_SERVICE_UUID         = 'c1a74048-c5c8-4e96-9720-ab28c66f12c0';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
 const SCORE_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 // PSDI Service UUID: Fixed value for Developer Trial
